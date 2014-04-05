e75f11f01135bc9efe66ba51c5109ebf547881d5
Nick Rioux <nickr500@gmail.com>
2014-04-05 17:12:33 -0400

    Requests

--- WelcomeMat/Lockpick/src/main/java/welcomemat/lockpick/Request.java
+++ WelcomeMat/Lockpick/src/main/java/welcomemat/lockpick/Request.java
@@ -19,26 +19,42 @@ public class Request {
         return Request.get(path, new HashMap<String, String>());
     }
 
+    private static void setRequestHeaders(HttpURLConnection http, Map<String, String> headers) {
+        for(String field : headers.keySet()) {
+            http.setRequestProperty(field, headers.get(field));
+        }
+    }
+
+    private static Map<String, String> getResponseHeaders(HttpURLConnection http) {
+        //gets first string using similar code to the for loop above
+        Map<String, String> firsts = new HashMap<String, String>();
+        for(String field : http.getHeaderFields().keySet()){
+            firsts.put(field, http.getHeaderFields().get(field).get(0));
+        }
+        return firsts;
+    }
+
     public static Response get(String path, Map<String, String> headers) throws IOException {
-        java.net.URL url = new URL(path);
+        URL url = new URL(path);
         HttpURLConnection http = (HttpURLConnection) url.openConnection();
         try {
-            for(String field : headers.keySet()) {
-                http.setRequestProperty(field, headers.get(field));
-            }
-            //gets first string using similar code to the for loop above
-            Map<String, String> firsts = new HashMap<String, String>();
-            for(String field : http.getHeaderFields().keySet()){
-                firsts.put(field, http.getHeaderFields().get(field).get(0));
-            }
-            Response magical = new Response(IOUtils.toString(http.getInputStream()), firsts);
-            return magical;
+            setRequestHeaders(http, headers);
+            return new Response(IOUtils.toString(http.getInputStream()), getResponseHeaders(http));
         } finally {
             http.disconnect();
         }
     }
 
-    public static Response post(String path, Map<String, String> data, Map<String, String> headers) {
-        return null;
+    public static Response post(String path, Map<String, String> data, Map<String, String> headers)
+            throws IOException {
+        URL url = new URL(path);
+        HttpURLConnection http = (HttpURLConnection) url.openConnection();
+        try {
+            setRequestHeaders(http, headers);
+
+        } finally {
+            http.disconnect();
+        }
+
     }
 }
