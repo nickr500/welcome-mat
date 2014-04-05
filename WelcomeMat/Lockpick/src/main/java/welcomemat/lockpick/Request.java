9ee0c162d07b6d582450e55fd35cc244bbbf0d0c
Nick Rioux <nickr500@gmail.com>
2014-04-05 14:59:18 -0400

    Begin requests

---
+++ WelcomeMat/Lockpick/src/main/java/welcomemat/lockpick/Request.java
@@ -0,0 +1,13 @@
+package welcomemat.lockpick;
+
+/**
+ * Created by nick on 4/5/14.
+ */
+public class Request {
+    public static Response get() {
+        return null;
+    }
+    public static Response post() {
+        return null;
+    }
+}
