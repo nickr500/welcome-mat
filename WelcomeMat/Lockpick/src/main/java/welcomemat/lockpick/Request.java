84f93806af09d2efe0ae3cd18d344053fccce84d
aalder <alder.a@husky.neu.edu>
2014-04-05 15:07:23 -0400

    committing?

--- WelcomeMat/Lockpick/src/main/java/welcomemat/lockpick/Request.java
+++ WelcomeMat/Lockpick/src/main/java/welcomemat/lockpick/Request.java
@@ -1,10 +1,31 @@
 package welcomemat.lockpick;
 
+import org.apache.commons.io.IOUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.helper.HttpConnection;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Map;
+
 /**
  * Created by nick on 4/5/14.
  */
 public class Request {
-    public static Response get() {
+    public static HttpConnection.Response get(String URL, Map<String, String> headers) throws IOException {
+        java.net.URL url = new URL(BASE_URL + path);
+        HttpURLConnection http = (HttpURLConnection) url.openConnection();
+        try {
+            Map<String, String> headsWillRoll = generateHeaders(cookie);
+            for(String field : headsWillRoll.keySet()) {
+                http.setRequestProperty(field, headsWillRoll.get(field));
+            }
+            IOUtils.toByteArray(http.getInputStream());
+        } finally {
+            http.disconnect();
+        }
         return null;
     }
     public static Response post() {
