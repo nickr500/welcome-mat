9b7caf2c52a3913ac82de11d3e656c58ab7b0db5
aalder <alder.a@husky.neu.edu>
2014-04-05 13:38:00 -0400

    added the python in comments

--- WelcomeMat/Lockpick/src/main/java/welcomemat/lockpick/Lockpick.java
+++ WelcomeMat/Lockpick/src/main/java/welcomemat/lockpick/Lockpick.java
@@ -21,8 +21,37 @@ public class Lockpick {
 
     // Allison
     String login(Config config) {
+        // Logs in to the huskycardcenter.neu.edu, returns the user's cookie
+        
         return null;
     }
+
+    /*def login(config):
+            """ Logins in to huskycardcenter.neu.edu, returns user cookie """
+
+            if args.verbose: print '[+] Logging in...'
+    sesstok, login_cookie = scrape('/login/ldap.php', None, True)
+    data = {
+        '__sesstok': sesstok,
+                'user': config['USER'],
+                'pass': config['PASS']
+    }
+    login = requests.post('{}/login/ldap.php'.format(BASE_URL),
+    data=data,
+    headers=generate_headers(login_cookie),
+    allow_redirects=False)
+    cookie = login.headers['set-cookie'].split(';')[0]
+
+            if args.verbose: print '\t[i] Got user cookie: {}'.format(cookie)
+
+    if test_login(cookie):
+            if args.verbose: print '[+] Login Successful!'
+            else:
+    print '[-] Login Failed!'
+    print '[-] Exiting'
+            sys.exit()
+
+            return cookie*/
 
     boolean testLogin(String cookie) {
 
